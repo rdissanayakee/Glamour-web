e766b8af2e9703c0bc9018a574f360bc6b20288c
Lahiru2579 <lahirulakshan0817@gmail.com>
2024-08-10 13:54:02 +0530

    adding js part to feedback cards

---
+++ index.js
@@ -0,0 +1,19 @@
+const feedbackContainer = document.querySelector('.feedback-container');
+const preFdbBtn = document.querySelector('.pre-fdb-btn');
+const nxtFdbBtn = document.querySelector('.nxt-fdb-btn');
+
+const feedbackCards = document.querySelectorAll('.feedback-card');
+
+let currentCardIndex = 0;
+function slideNext() {
+  currentCardIndex = (currentCardIndex + 1) % feedbackCards.length;
+  feedbackContainer.scrollLeft = feedbackCards[currentCardIndex].offsetLeft;
+}
+
+function slidePrev() {
+  currentCardIndex = (currentCardIndex - 1 + feedbackCards.length) % feedbackCards.length;
+  feedbackContainer.scrollLeft = feedbackCards[currentCardIndex].offsetLeft;
+}
+
+preFdbBtn.addEventListener('click', slidePrev);
+nxtFdbBtn.addEventListener('click', slideNext);
