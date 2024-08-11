9d9830a366a708ea48363f11d25400b634fbb64c
Lahiru2579 <lahirulakshan0817@gmail.com>
2024-08-11 16:22:17 +0530

    Trending Items

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ $(document).ready(function () {
   });
 });
 
+
+
+
+
+
 // Heading Animation
 const animatedHeading = document.querySelector('.animated-heading');
 animatedHeading.classList.add('show');
@@ -31,3 +36,34 @@ function slidePrev() {
 
 preFdbBtn.addEventListener('click', slidePrev);
 nxtFdbBtn.addEventListener('click', slideNext);
+
+
+
+
+
+
+
+
+//products animation
+document.addEventListener("DOMContentLoaded", function() {
+  const observerOptions = {
+    root: null, // Use the viewport as the container
+    rootMargin: "0px",
+    threshold: 0.2 // Trigger when 20% of the element is visible
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          entry.target.classList.add("animate");
+          observer.unobserve(entry.target);
+        }, index * 100); // Delay each item by 100ms
+      }
+    });
+  }, observerOptions);
+
+  document.querySelectorAll('.product-item').forEach(item => {
+    observer.observe(item);
+  });
+});
