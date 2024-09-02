178c792a79c96a68c26a7ada250a3b4e216c0553
DinushaS14 <sdinusha902@gmail.com>
2024-09-02 16:04:36 +0530

    change to php extension

--- men's loungewear.php
+++ men's loungewear.php
@@ -59,10 +59,10 @@
   <!-- Secondary Navigation Bar -->
 <nav class="secondary-navbar">
     <ul class="nav-links1">
-        <li><a href="men's t-shirt.html">T-SHIRTS</a></li>
-        <li><a href="men's bottom.html">BOTTOMS</a></li>
-        <li><a href="men's loungewear.html">LOUNGEWEAR</a></li>
-        <li><a href="men's shoes.html">FOOTWEAR</a></li>
+        <li><a href="men's t-shirt.php">T-SHIRTS</a></li>
+        <li><a href="men's bottom.php">BOTTOMS</a></li>
+        <li><a href="men's loungewear.php">LOUNGEWEAR</a></li>
+        <li><a href="men's shoes.php">FOOTWEAR</a></li>
        
     </ul>
 </nav>
