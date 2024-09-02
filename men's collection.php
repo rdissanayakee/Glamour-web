6ce05b8d1f0d56c88722627f385a632a6f089e33
DinushaS14 <sdinusha902@gmail.com>
2024-09-02 20:23:23 +0530

    change php extension

--- men's collection.php
+++ men's collection.php
@@ -15,7 +15,7 @@
 
     <link rel="stylesheet" href="men's collection.css">
     <script src="men's collection.js"></script>
-    <script src="men's collection.js"></script>
+    
     <style>
         a{
             text-decoration: none !important;
@@ -29,11 +29,11 @@
     <nav class="navbar">
         <div class="container">
             <div class="left-section">
-                <a href="index.html" class="brand">Glamour</a>
+                <a href="index.php" class="brand">Glamour</a>
                 <ul class="nav-links">
                     <li><a href="men's collection.php">MEN</a></li>
                     <li><a href="women's collection.php">WOMEN</a></li>
-                    <li><a href="kids.phpl">KIDS</a></li>
+                    <li><a href="kids.php">KIDS</a></li>
                     <li><a href="Home&Deco.php">HOME & DECO</a></li>
                     <li><a href="accesories.php">ACCESSORIES</a></li>
                     <li><a href="personalcare.main.php">PERSONAL CARE</a></li>
@@ -48,7 +48,7 @@
             </div>
             <div class="right-section">
                 <a href="cart.html" class="nav-icon"><i class="fas fa-shopping-cart"></i></a>
-                <a href="login.html" class="nav-icon"><i class="fas fa-user"></i> Login</a>
+                <a href="login1.php" class="nav-icon"><i class="fas fa-user"></i> Login</a>
             </div>
         </div>
     </nav>
@@ -309,11 +309,11 @@
 					<div class="footer-section">
 						<h3>COMPANY & OUR POLICIES</h3>
 						<ul>
-							<li><a href="contact us.html">Contact Us</a></li>
-							<li><a href="contact us.html">Privacy Policy</a></li>
-							<li><a href="contact us.html">Shipping Policy</a></li>
-							<li><a href="contact us.html">Returns & Exchanges Policy</a></li>
-							<li><a href="contact us.html">Terms of Service</a></li>
+							<li><a href="contact us.php">Contact Us</a></li>
+							<li><a href="contact us.php">Privacy Policy</a></li>
+							<li><a href="contact us.php">Shipping Policy</a></li>
+							<li><a href="contact us.php">Returns & Exchanges Policy</a></li>
+							<li><a href="contact us.php">Terms of Service</a></li>
 						</ul>
 					</div>
 					<div class="footer-section">
